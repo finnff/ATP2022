0f5919c012782d943a028ca805b39f064c196d21
finn <finn@fonteijn.fi>
2023-08-29 22:17:53 +0000

    dashboard + reality is working

--- src/dashboard.py
+++ src/dashboard.py
@@ -1,1 +1,150 @@
+import dash
+import dash_bootstrap_components as dbc
+from dash import dcc, html
+from dash.dependencies import Input, Output
+import plotly.graph_objs as go
+import redis
+from collections import deque
+import random
 
+
+# Initialize Redis client
+r = redis.Redis(host='localhost', port=6379, db=0)
+UPDATE_RATE = 500
+
+# Initialize the Dash app
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.DARKLY])
+
+
+
+def random_color():
+    return "#{:02x}{:02x}{:02x}".format(
+        random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)
+    )
+
+
+
+
+def random_color():
+    return "#{:02x}{:02x}{:02x}".format(
+        random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)
+    )
+
+class GraphGenerator:
+    def __init__(self, hash_name, redis_client, max_points=25):
+        self.hash_name = hash_name
+        self.data = {}
+        self.colors = {}
+        self.max_points = max_points
+        self.redis_client = redis_client
+
+    def update_graph(self):
+        for key, value in self.redis_client.hgetall(self.hash_name).items():
+            key_str = key.decode('utf-8')
+            try:
+                value_float = float(value.decode('utf-8'))
+
+                if key_str not in self.data:
+                    self.data[key_str] = deque([None] * self.max_points, maxlen=self.max_points)
+                    self.colors[key_str] = random_color()
+
+                self.data[key_str].append(value_float)
+            except ValueError:
+                pass
+
+        traces = []
+        for key, values in self.data.items():
+            traces.append(
+                go.Scatter(
+                    x=list(range(self.max_points)),
+                    y=list(values),
+                    mode='lines+markers',
+                    name=f"{self.hash_name}_{key}",
+                    line=dict(color=self.colors[key])
+                )
+            )
+
+        layout = go.Layout(
+            title=f'{self.hash_name} Over Time',
+            xaxis=dict(title='Time', range=[0, self.max_points - 1]),
+            yaxis=dict(title='Value')
+        )
+
+        return {'data': traces, 'layout': layout}
+
+# Generate graphs
+#
+graph1 = GraphGenerator('RealitySimReplay',r, max_points=25)
+graph2 = GraphGenerator('Sensor_Actuator',r, max_points=25)
+graph3 = GraphGenerator('sim_stat', r,max_points=25)
+
+# Dash Layout
+app.layout = dbc.Container([
+    dbc.Row([
+        dbc.Col(dcc.Graph(id='graph-A'), width=4),
+        dbc.Col(dcc.Graph(id='graph-B'), width=4),
+        dbc.Col(dcc.Graph(id='graph-C'), width=4),
+    ]),
+    dbc.Row([
+        dbc.Col([
+            html.Div(id='current-values-D'),
+            dcc.Input(id='input-D1', type='number', value=0),
+            dcc.Input(id='input-D2', type='number', value=0),
+            dcc.Input(id='input-D3', type='number', value=0),
+            dcc.Input(id='input-D4', type='number', value=0),
+        ], width=4),
+        dbc.Col([
+            html.Div(id='current-values-E'),
+            dcc.Input(id='input-E1', type='number', value=0),
+            dcc.Input(id='input-E2', type='number', value=0),
+            dcc.Input(id='input-E3', type='number', value=0),
+            dcc.Input(id='input-E4', type='number', value=0),
+        ], width=4),
+        dbc.Col([
+            html.Div(id='current-values-F'),
+            dcc.Slider(id='input-F1', min=0.1, max=10, step=0.1, value=1),
+            dcc.Input(id='input-F2', type='number', value=0),
+            dcc.Input(id='input-F3', type='number', value=0),
+            dcc.Input(id='input-F4', type='number', value=0),
+        ], width=4),
+    ]),
+    dcc.Interval(id='interval-update', interval=1*UPDATE_RATE, n_intervals=0)
+])
+
+# Update the graphs
+
+@app.callback(
+    [Output('graph-A', 'figure'),
+     Output('graph-B', 'figure'),
+     Output('graph-C', 'figure')],
+    Input('interval-update', 'n_intervals')
+)
+def update_all_graphs(n):
+    return graph1.update_graph(), graph2.update_graph(), graph3.update_graph()
+
+
+# Update current hash values
+@app.callback(
+    [Output('current-values-D', 'children'),
+     Output('current-values-E', 'children'),
+     Output('current-values-F', 'children')],
+    Input('interval-update', 'n_intervals')
+)
+def update_current_values(n):
+    def get_current_values(hash_name):
+        values = []
+        for key, value in r.hgetall(hash_name).items():
+            try:
+                values.append(f"{key.decode('utf-8')}: {float(value.decode('utf-8'))},\n")
+            except ValueError:
+                pass
+        return html.Div(values)
+
+    current_values_D = get_current_values('Sensor_Actuator')
+    current_values_E = get_current_values('sim_stat')
+    current_values_F = get_current_values('RealitySimReplay')
+    
+    return current_values_D, current_values_E, current_values_F
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
--- src/reality.py
+++ src/reality.py
@@ -1,6 +1,7 @@
 import redis
 import os
 import random
+import math
 import time
 import json
 
@@ -21,6 +22,14 @@ class CarParams:
         self.wheel_diameter = wheel_diameter
         self.encoder_cpr = encoder_cpr
         self.seconds_distance = seconds_distance
+    def __str__(self):
+        return (f"Using Parameters: {self.param_name}\n"
+                f"MaxAcc: {self.max_acceleration}\n"
+                f"MaxDec: {self.max_deceleration}\n"
+                f"Wheel_size: {self.wheel_diameter}\n"
+                f"WheelSpeed CPR: {self.encoder_cpr}\n"
+                f"Sec Dist: {self.seconds_distance}\n\n")
+
 
 
 def load_car_from_json(file_path):
@@ -45,10 +54,16 @@ def load_all_cars(folder_path):
     return Loaded_Car_Parameters
 
 
+def set_sim_params(redis, speed=1.0, frequency=100, update_frequency=1.0):
+    redis.hset("RealitySimReplay", "RealityTimeScaleFactor", speed)
+    redis.hset("RealitySimReplay", "reality_frequency", frequency)
+    redis.hset("RealitySimReplay", "reality_update_frequency", update_frequency)
+
+
 def get_sim_params(redis):
-    A = float(redis.hget("RealitySimReplay", "RealityTimeScaleFactor"))
-    B = float(redis.hget("RealitySimReplay", "reality_frequency"))
-    C = float(redis.hget("RealitySimReplay", "reality_update_frequency"))
+    A = float(redis.hget("RealitySimReplay", "RealityTimeScaleFactor") or -1.0)
+    B = float(redis.hget("RealitySimReplay", "reality_frequency") or -1.0)
+    C = float(redis.hget("RealitySimReplay", "reality_update_frequency") or -1.0)
     return A, B, C
 
 
@@ -63,13 +78,14 @@ class Reality:
         redis_client,
     ):
         self.car_parameters = car_parameters
+        print(self.car_parameters)
         self.true_vehicle_speed = true_vehicle_speed
         self.true_distance_to_voorligger = true_distance_to_voorligger
         self.true_voorligger_speed = true_voorligger_speed
         self.true_vehicle_acceleration = true_vehicle_acceleration
         self.GasRemPedalPosPercentage = 0.0
         self.iteration = 0
-        self.time_since_reality_update = 0.11
+        self.time_since_reality_update = 0.01
         self.radar_counter = 0
         self.redis = redis_client
         print(
@@ -84,41 +100,58 @@ class Reality:
         ) = get_sim_params(self.redis)
 
     def save_state_to_redis(self):
-        self.redis.hset(
-            "sim_state", "true_distance_to_voorligger", self.true_distance_to_voorligger
-        )
+        self.redis.hset("sim_state", "true_distance_to_voorligger", self.true_distance_to_voorligger)
         self.redis.hset("sim_state", "true_vehicle_speed", self.true_vehicle_speed)
-        self.redis.hset(
-            "Sensor_Actuator", "GasRemPedalPosPercentage", self.GasRemPedalPosPercentage
-        )
+        self.redis.hset("sim_state", "true_voorligger_speed", self.true_voorligger_speed)
+        self.redis.hset("sim_state", "true_vehicle_acceleration", self.true_vehicle_acceleration)
         self.redis.hset("RealitySimReplay", "timeLoop_us", (time.time() - start_time))
 
     def load_state_from_redis(self):
-        self.true_distance_to_voorligger = float(
-            self.redis.hget("sim_state", "true_distance_to_voorligger") or 0.0
-        )
-        self.true_vehicle_speed = float(
-            self.redis.hget("sim_state", "true_vehicle_speed") or 0.0
-        )
-        self.GasRemPedalPosPercentage = float(
-            self.redis.hget("Sensor_Actuator", "GasRemPedalPosPercentage") or 0.0
-        )
+        self.true_distance_to_voorligger = float(self.redis.hget("sim_state", "true_distance_to_voorligger") or 0.0)
+        self.true_vehicle_speed = float(self.redis.hget("sim_state", "true_vehicle_speed") or 0.0)
+        self.true_voorligger_speed = float(self.redis.hget("sim_state", "true_voorligger_speed") or 0.0)
+        self.true_vehicle_acceleration = float(self.redis.hget("sim_state", "true_vehicle_acceleration") or 0.0)
+        self.GasRemPedalPosPercentage = float(self.redis.hget("Sensor_Actuator", "GasRemPedalPosPercentage") or 0.0)
+
 
     def alternate_voorligger_speed(self):
         if self.iteration % 2 == 0:
             self.true_voorligger_speed = 110
         else:
-            self.true_voorligger_speed = 80
+            self.true_voorligger_speed = 90
 
-    def dummyHzWheelSpeedSensor(self):
-        print("xx")
-        # 1. takes   true_vehicle_speed (in meter per second)(use scaled_elapsed_time)
-        # 2 self.car_parameters.wheel_diameter * pi for length wheel
-        # 3we can now calculate with all the time and scaled_elapsed_time how much wheel roations per timeunit(sub
-        # second scaled time something) we get.
-        # 4 self.car_parameters.encoder_cpr can be multiplied by this R/S for magnetic encoder pulses per timeunit..
-        # 5  this number back to seconds to get Hz, we then ad random.choice( +- 0.3%) and Redis set to Sensor_Actuator
-        pass
+    def dummyHzWheelSpeedSensor(self, scaled_elapsed_time):
+        # Step 1: Get true_vehicle_speed in meters per second
+        speed_mps = self.true_vehicle_speed
+        # Step 2: Calculate wheel circumference in meters
+        wheel_circumference = math.pi * self.car_parameters.wheel_diameter
+        # Step 3: Calculate rotations per scaled time unit
+        rotations_per_time_unit = (
+            speed_mps * scaled_elapsed_time
+        ) / wheel_circumference
+        # Step 4: Calculate encoder pulses per time unit
+        encoder_pulses_per_time_unit = (
+            rotations_per_time_unit * self.car_parameters.encoder_cpr
+        )
+        # Step 5: Convert this to Hz (pulses per second)
+        encoder_pulses_per_second = encoder_pulses_per_time_unit / scaled_elapsed_time
+        encoder_pulses_per_second *= 1 + random.choice(
+            [-0.003, 0.003]
+        )  # Add random choice of +- 0.3%
+        # Step 6: Save to Redis
+        self.redis.hset(
+            "Sensor_Actuator", "WheelSpeedSensorHz", encoder_pulses_per_second
+        )
+        # Create f-string explaining the math and send that to Redis
+        explanation = (
+            f"Speed (m/s): {speed_mps}, "
+            f"Wheel Diameter (m): {self.car_parameters.wheel_diameter}, "
+            f"Wheel Circumference (m): {wheel_circumference}, "
+            f"Rotations/Time Unit: {rotations_per_time_unit}, "
+            f"Encoder Pulses/Time Unit: {encoder_pulses_per_time_unit}, "
+            f"Encoder Pulses/s (Hz): {encoder_pulses_per_second}"
+        )
+        self.redis.hset("Sensor_Actuator", "WheelSpeedSensorExplanation", explanation)
 
     def bosch_radar_update(self):
         if self.radar_counter >= 500:  # Check if 500ms have passed
@@ -174,6 +207,7 @@ class Reality:
                 )
                 print(values_line, end="\r")
             self.bosch_radar_update()
+            self.dummyHzWheelSpeedSensor(scaled_elapsed_time)
         else:
             self.time_since_reality_update += elapsed_time  # ?
 
@@ -181,32 +215,29 @@ class Reality:
 if __name__ == "__main__":
     redis_client = redis.StrictRedis(host="localhost", port=6379, db=0)
 
-    speed_scale_factor, reality_frequency, reality_update_frequency = get_sim_params(
-        redis_client
-    )
-
+    set_sim_params(redis_client, speed=1.00, frequency=100.00, update_frequency=1.00)
     Loaded_Car_Parameters = load_all_cars("./car_constants")
     print(f"Loaded car parameters for: {[i.param_name for i in Loaded_Car_Parameters]}")
-    default_car_param = Loaded_Car_Parameters[0] if Loaded_Car_Parameters else None
+    reality = Reality(Loaded_Car_Parameters[0], 100, 2000, 100, 0, redis_client)
+    time.sleep(10)
+    (
+        speed_scale_factor,
+        reality_frequency,
+        reality_update_frequency,
+    ) = get_sim_params(redis_client)
 
-    if default_car_param:
-        reality = Reality(default_car_param, 100, 2000, 100, 1, redis_client)
-        last_update_time = time.time()
+    last_update_time = time.time()
+    while True:
+        start_time = time.time()
+        reality.load_state_from_redis()
 
-        while True:
-            start_time = time.time()
-            reality.load_state_from_redis()
+        elapsed_time = (time.time() - start_time) * reality_frequency
+        last_update_time = start_time
 
-            elapsed_time = (time.time() - start_time) * reality_frequency
-            last_update_time = start_time
+        reality.update(elapsed_time)
+        reality.save_state_to_redis()
 
-            reality.update(elapsed_time)
-            reality.save_state_to_redis()
-
-            sleep_duration = max(
-                0, 1 / reality_frequency - elapsed_time
-            )  # Ensure a cycle as per reality_frequency
-            time.sleep(sleep_duration)
-
-    else:
-        print("No car parameters loaded. Exiting.")
+        sleep_duration = max(
+            0, 1 / reality_frequency - elapsed_time
+        )  # Ensure a cycle as per reality_frequency
+        time.sleep(sleep_duration)
